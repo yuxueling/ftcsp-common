fdf94b7619575df7bba1594f0c799160714798bd
Hzof <Hzof@DESKTOP-TVVV7ID>
2018-07-26 09:49:37 +0800

    添加了一个文件来源代码

---
+++ src/main/java/com/sxyht/common/interfaces/FileSourceEnum.java
@@ -0,0 +1,16 @@
+package com.sxyht.common.interfaces;
+
+public interface FileSourceEnum {
+	/**
+	 * 公共的，不需要此字段时填入
+	 */
+	Integer COMMON =1;
+	/**
+	 * 客户合同附件的代码
+	 */
+	Integer CLIENT_CONTRACT_ATTACHMENT=2;
+	/**
+	 * 进仓通知书文件的代码
+	 */
+	Integer EX_ORDER_ADMISSION_NOTICE =3;
+}
