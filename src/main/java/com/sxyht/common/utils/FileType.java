448dd9d57df34ca61373c8904943411b04cff23f
Hzof <Hzof@DESKTOP-TVVV7ID>
2018-10-24 10:43:53 +0800

    文件类型的新增

--- src/main/java/com/sxyht/common/utils/FileType.java
+++ src/main/java/com/sxyht/common/utils/FileType.java
@@ -8,13 +8,14 @@ package com.sxyht.common.utils;
  * 备注：文件类型不完善，有需要的自行添加
  */
 public class FileType {
-	public static final String[] img = { "bmp", "jpg", "jpeg", "png", "tiff", "gif", "pcx", "tga", "exif", "fpx", "svg", "psd",
+	public static final String[] PICTURE = { "bmp", "jpg", "jpeg", "png", "tiff", "gif", "pcx", "tga", "exif", "fpx", "svg", "psd",
 			"cdr", "pcd", "dxf", "ufo", "eps", "ai", "raw", "wmf" };
-	public static final String[] document = { "txt", "doc", "docx", "xls", "htm", "html", "jsp", "rtf", "wpd", "pdf", "ppt" };
-	public static final String[] video = { "mp4", "avi", "mov", "wmv", "asf", "navi", "3gp", "mkv", "f4v", "rmvb", "webm" };
-	public static final String[] music = { "mp3", "wma", "wav", "mod", "ra", "cd", "md", "asf", "aac", "vqf", "ape", "mid", "ogg",
+	public static final String[] DOCUMENT = { "txt", "doc", "docx", "xls", "htm", "html", "jsp", "rtf", "wpd", "pdf", "ppt" };
+	public static final String[] VIDEO = { "mp4", "avi", "mov", "wmv", "asf", "navi", "3gp", "mkv", "f4v", "rmvb", "webm" };
+	public static final String[] MUSIC = { "mp3", "wma", "wav", "mod", "ra", "cd", "md", "asf", "aac", "vqf", "ape", "mid", "ogg",
 			"m4a", "vqf" };
-	public static final String[] sql= {"sql"};
+	public static final String[] SCRIPT= {"sql"};
+	public static final String[] COMPRESS = {"zip"};
 	public static int fileType(String fileName) {
 		if (fileName == null) {
 			fileName = "文件名为空！";
@@ -22,17 +23,19 @@ public class FileType {
 		}
 		// 获取文件后缀名并转化为小写，用于后续比较
 		String fileType = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length()).toLowerCase();
-		for (int i = 0; i < img.length; i++) {
-			if (img[i].equals(fileType)) 
+		for (int i = 0; i < PICTURE.length; i++) {
+			if (PICTURE[i].equals(fileType)) 
 				return 0;
-			if (document[i].equals(fileType)) 
+			if (DOCUMENT[i].equals(fileType)) 
 				return 1;
-			if (video[i].equals(fileType)) 
+			if (VIDEO[i].equals(fileType)) 
 				return 2;
-			if (music[i].equals(fileType)) 
+			if (MUSIC[i].equals(fileType)) 
 				return 3;
-			if(sql[i].equals(fileType))
+			if(SCRIPT[i].equals(fileType))
 				return 4;
+			if(COMPRESS[i].equals(fileType))
+				return 5;
 		}
 		//4
 		return 99;
